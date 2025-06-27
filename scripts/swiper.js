c2e038f892d756b01c5544186f915d580959b697
KevDeto <contactohuayta@gmail.com>
2025-06-26 21:18:17 -0300

    se termino el header, el carrusel y las categorias de productos en el main

---
+++ scripts/swiper.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const swiperHome = new Swiper(".seccion-carrusel-home .swiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
