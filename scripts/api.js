54744c2cbde608dfec1db21e3dc8714729a53feb
KevDeto <contactohuayta@gmail.com>
2025-07-01 20:16:50 -0300

    terminado el footer, la redireccion a los preductos segun la categoria (del main), se consumio la api y se termino de aplicar la funcion local storage, se aplico el header fijo, se cambio el body para que funcione con grid

---
+++ scripts/api.js
@@ -0,0 +1,83 @@
+let productos = [
+  /*   {
+    "nombre": "nombre 1",
+    "precio": "precio 1",
+    "cantCuotas": "cantCuotas 1",
+    "precioCuotas": "precioCuotas 1",
+    "imagen": [
+      "https://res.cloudinary.com/dljovimrs/image/upload/v1751000640/lampara1_jodovk.webp",
+      "https://res.cloudinary.com/dljovimrs/image/upload/v1751000642/lampara2_gqxjri.webp",
+      "https://res.cloudinary.com/dljovimrs/image/upload/v1751000644/lampara3_u7wdal.webp",
+      "https://res.cloudinary.com/dljovimrs/image/upload/v1751000647/lampara4_iqcj6m.webp"
+    ],
+    "categoria": "iluminacion",
+    "estaEnDescuento": true,
+    "descuento": "45",
+    "id": "1"
+  }*/
+];
+
+const categoriaUrl = new URLSearchParams(window.location.search);
+const categoriaActual = categoriaUrl.get("categoria");
+
+async function fetchProductos() {
+  try {
+    const nombreCache = `cacheProducto_${categoriaActual}`;
+    const productosCacheados = localStorage.getItem(nombreCache);
+    if (productosCacheados) {
+      const jsonProductosCache = JSON.parse(productosCacheados);
+      productos = jsonProductosCache.products;
+
+      renderizarProductos(categoriaActual);
+      return;
+    }
+
+    const response = await fetch(`https://dummyjson.com/products/category/${categoriaActual}`);
+    const jsonProductos = await response.json();
+    productos = jsonProductos.products;
+    localStorage.setItem(nombreCache, JSON.stringify(jsonProductos));
+    console.log(productos);
+
+    renderizarProductos(categoriaActual);
+  } catch (error) {
+    console.error("Error al obtener productos:", error);
+    document.getElementById("seccion-productos").innerHTML = `
+      <p class="error">No se pudieron cargar los productos. Intente recargar.</p>
+    `;
+  }
+}
+
+function renderizarProductos(categoria) {
+  const productosFiltrados = productos.filter((producto) => producto.category === categoria);
+  console.log(productosFiltrados)
+  const seccion = document.getElementById("seccion-productos");
+
+  seccion.innerHTML = "";
+
+  const contenedor = document.createElement("div");
+  contenedor.className = "contenedor-tarjetas";
+
+  contenedor.innerHTML = productosFiltrados
+    .map(
+      (producto) => `
+      <a href="#" class="tarjeta-producto-link">
+        <div class="tarjeta-producto"> 
+          <div class="contenedor-imagen">
+            <img src="${producto.thumbnail}" alt="${producto.title}" class="imagen-producto">
+            <span class="descuento-producto">${producto.discountPercentage}% OFF</span>
+          </div>
+          <div class="contenedor-informacion">
+            <h3 class="titulo-producto">${producto.title}</h3>
+            <p class="precio-producto">$${producto.price}</p>
+            <p class="cuotas-producto">Cuotas: ${producto.cantCuotas}</p>
+          </div>
+        </div>
+      </a>
+    `
+    )
+    .join("");
+  /* ${producto.discountPercentage ?   : ""}*/
+  seccion.appendChild(contenedor);
+}
+
+document.addEventListener("DOMContentLoaded", fetchProductos);
--- scripts/swiper.js
+++ scripts/swiper.js
@@ -3,12 +3,28 @@ document.addEventListener("DOMContentLoaded", function () {
     slidesPerView: 1,
     loop: true,
     pagination: {
-      el: ".swiper-pagination",
+      el: ".seccion-carrusel-home .swiper-pagination",
       clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".seccion-carrusel-home .swiper-button-next",
+      prevEl: ".seccion-carrusel-home .swiper-button-prev",
+    },
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const swiperDescuentos = new Swiper(".seccion-carrusel-descuentos .swiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".seccion-carrusel-descuentos .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".seccion-carrusel-descuentos .swiper-button-next",
+      prevEl: ".seccion-carrusel-descuentos .swiper-button-prev",
     },
   });
 });
